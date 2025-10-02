276895e2b57ed2c883bc5961ccbf3bd56c0e1021
Toni D <tonidy@outlook.com>
2025-10-03 00:02:48 +0700

    chore: fix error

--- facades/openapi.go
+++ facades/openapi.go
@@ -1,17 +1,27 @@
 package facades
 
 import (
-	"log"
-
 	"github.com/TickLabVN/tonic/core/docs"
-	"github.com/goravel/framework/facades"
+	"github.com/tonidy/goravel-ginx"
 )
 
 func OpenApi() *docs.OpenApi {
-	instance, err := facades.App().Make("goravel.gin.openapi")
+	// Return the global instance if available
+	if gin.OpenApiInstance != nil {
+		return gin.OpenApiInstance
+	}
+
+	// Try to create from container
+	instance, err := gin.App.Make(gin.BindingOpenApi)
 	if err != nil {
-		log.Fatalln(err)
-		return nil
+		// Return a default instance if container not ready
+		return &docs.OpenApi{
+			OpenAPI: "3.0.1",
+			Info: docs.InfoObject{
+				Version: "1.0.0",
+				Title:   "API Documentation",
+			},
+		}
 	}
 
 	return instance.(*docs.OpenApi)
